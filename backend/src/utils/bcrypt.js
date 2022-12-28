54d87a86896df55c6262fe3705c08201d1e74f85
JiahuanLiu13 <cheayuki13@gmail.com>
2022-12-28 15:10:49 +0800

    [后端]bcrypt工具hash加密

--- backend/src/utils/bcrypt.js
+++ backend/src/utils/bcrypt.js
@@ -1,34 +1,73 @@
-//数据加密
-const md5 = require('md5')
+//数据加密 md5方式
+// const md5 = require('md5')
 
-const HACK = 'HACK'
+// const HACK = 'HACK'
 
-const md5Passwd = (passwd)=>{
+// const md5Passwd = (passwd)=>{
+//     return new Promise((resolve,reject)=>{
+//         const md5PWD = md5(passwd + HACK)
+//         resolve(md5PWD)
+//     })
+// }
+
+
+// const matchPasswd = (oldMd5PWD,passwd)=>{
+//     return new Promise((resolve,reject)=>{
+//         const newMd5PWD = md5(passwd + HACK)
+//         if(oldMd5PWD === newMd5PWD){
+//             resolve(true)
+//         }else{
+//             reject(false)
+//         }
+//     })
+// }
+
+// module.exports = md5Passwd
+// module.exports = matchPasswd
+
+// async function test(){
+//     const passwd = 'abcd'
+//     const md5pwd = md5Passwd(passwd)
+//     console.log('md5pwd',md5pwd);
+
+// }
+// test()
+
+//或者直接用 npm的 bcrypt
+
+const bcrypt = require('bcrypt')
+
+const SALT = 10
+
+//加密
+const hashPasswd = (passwd)=>{
     return new Promise((resolve,reject)=>{
-        const md5PWD = md5(passwd + HACK)
-        resolve(md5PWD)
+        bcrypt.hash(passwd,SALT,(err,encrypted)=>{
+            if(err){
+                reject(err)
+            }
+            resolve(encrypted)
+        })
     })
 }
 
 
-const matchPasswd = (oldMd5PWD,passwd)=>{
-    return new Promise((resolve,reject)=>{
-        const newMd5PWD = md5(passwd + HACK)
-        if(oldMd5PWD === newMd5PWD){
-            resolve(true)
-        }else{
-            reject(false)
-        }
+//比对
+const matchPasswd = (oldhashPWD,passwd)=>{
+    return new Promise(async (resolve,reject)=>{
+        const match = await bcrypt.compare(passwd, oldhashPWD)
+        console.log(match)
     })
 }
 
-module.exports = md5Passwd
-module.exports = matchPasswd
+module.exports = {matchPasswd,hashPasswd}
 
-async function test(){
+//测试2
+/* async function test2(){
     const passwd = 'abcd'
-    const md5pwd = md5Passwd(passwd)
-    console.log('md5pwd',md5pwd);
-
+    const hashpwd = await hashPasswd(passwd)
+    console.log('md5pwd',hashpwd);
+    const match = await matchPasswd(hashpwd,'abcd')
+    console.log("passwd match",match)
 }
-test()
+test2() */
