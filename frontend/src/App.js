d89ca7768998e1c4bb54b628acad54e42b9afe2a
JiahuanLiu13 <cheayuki13@gmail.com>
2023-01-06 02:16:40 +0800

    fix,v6版本的后代路由，找了好久真无语“https://www.reactrouter.cn/docs/getting-started/overview#%E5%A4%9A%E7%BB%84%E8%B7%AF%E7%94%B1”

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div className="App">
-      App component 666
-    </div>
-  );
+import { Component } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+      </div >
+    )
+  }
+
+
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';    //这里使用的是v6，v6 switch变为Routes，Route的component变为element，两者不兼容，注意。
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';    
+import { BrowserRouter, Route, Routes } from 'react-router-dom';//这里使用的是v6，v6 switch变为Routes，Route的component变为element，两者不兼容，注意。
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />} /> 
+            <Route path="/*" element={<App />} /> 
         </Routes>
     </BrowserRouter>
 )
